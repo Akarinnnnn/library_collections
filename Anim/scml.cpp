72aec1eafb64f05958db81913e6ce1990df9b044
Akarinnnnn <1207247008@qq.com>
2019-03-21 18:51:54 +0800

    ++

---
+++ Anim/scml.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "scml.h"
+#include <pugiconfig.hpp>
+#include <pugixml.hpp>
+
+using namespace Anim::SCML;
+using namespace std;
+scml Anim::SCML::ReadSCML(std::wstring path)
+{
+	using namespace pugi;
+	return scml();
+}
--- Anim/scml.h
+++ Anim/scml.h
@@ -2,51 +2,84 @@
 #include <string>
 #include <vector>
 #include <filesystem>
-#include <pugiconfig.hpp>
-#include <pugixml.hpp>
 namespace Anim
 {
-	namespace SCML
+	namespace SCML//kleiÌØ»¯
 	{
-		class scmlReader
-		{
-		public:
-			scmlReader(std::wstring path);
-			~scmlReader();
-
-		private:
-			pugi::xml_document scmldocument;
-		};
 		class file
 		{
 		public:
 			unsigned int id = 0;
 			unsigned int w, h = 0;
-			float x, y = 0.0;
+			float x, y = 0.0F;
 			std::wstring name;
-			std::filesystem::path file;
+			std::filesystem::path filepath;
 		};
 		class folder
 		{
 		public:
 			std::vector<file> files;
 			std::wstring name;
+			unsigned int id=0;
+		};
+		struct object_ref
+		{
+			unsigned int id = 0;
+			std::wstring name;
+			float x, y = 0.0f;
+			float angle = 0.0f;//radius angle
+		};
+		struct object
+		{
+			unsigned int id = 0;
+			unsigned int folder,file = 0;
+		};
+		class key
+		{
+			std::vector<object> objects;
 			unsigned int id;
+		};
+		class key_mainline
+		{
+		public:
+			std::vector<object_ref> objects;
+			unsigned int id;
+		};
+		class timeline
+		{
+		public:
+			std::vector<key_mainline> keys;
+			unsigned int id=0;
+			unsigned int length=0;
+		};
+		class _mainline
+		{
+		public:
+			std::vector<key> keys;
+			std::wstring name;
+			unsigned int length;//frame
 		};
 		class animation
 		{
 		public:
+			_mainline mainline;
+			std::vector<timeline> timelines;
 			std::wstring name;
-			unsigned int id;
-			unsigned int length;
+			unsigned int id=0;
+			unsigned int length=0;//frame
 		};
 		class entity
 		{
 		public:
 			std::wstring name;
-			animation mainline;
 			std::vector<animation> animations;
-			unsigned int id;
+			unsigned int id=0;
 		};
+		struct scml
+		{
+			std::vector<folder> folder;
+			std::vector<entity> entity;
+		};
+		scml ReadSCML(std::wstring path);
 	}
 }
